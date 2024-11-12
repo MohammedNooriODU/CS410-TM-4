fe5271b4248780f85e90f8d1f32308934aacbdaa
Josh Hardy <nii.san156@gmail.com>
2024-11-12 15:24:01 -0500

    Did the Commenting out of the nex.config.mjs and added to the gitignore

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  //   distDir: "dist",
-  basePath: "/CS410-TM-4",
-  images: {
-    unoptimized: true,
-  },
+  // output: "export",
+  // //   distDir: "dist",
+  // basePath: "/CS410-TM-4",
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 export default nextConfig;
