a0db39710b5acacf7c7372b20ea694441f5d999e
Mohammed Noori <mnoor002@odu.edu>
2024-11-12 19:28:29 -0500

    add feasibility slide

--- src/app/presentations/feasibility/page.js
+++ src/app/presentations/feasibility/page.js
@@ -1,12 +1,17 @@
 export default function Feasibility() {
   return (
-    <div className="hero bg-base-300 min-h-screen bg-transparent">
-      <div className="hero-content text-left bg-white border-purple-500 animated-gradient rounded-3xl p-16 border-4">
-        <div className="max-w-md text-black flex flex-col justify-center contents-center">
-          <h1 className="text-5xl font-bold">The Feasibility</h1>
-          <iframe src="https://docs.google.com/presentation/d/e/2PACX-1vQAw5-8V2xrkrYlycTMhnIosr_IwMWLlGy_N9HzUQq_3p90dqG6qAomO0JRD8r0e7RcD8q1na9ufneH/embed?start=false&loop=false&delayms=3000" frameBorder="0" width="960" height="569" allowFullScreen={true} mozallowfullscreen="true" webkitallowfullscreen="true">
-          </iframe>  
-        </div>
+    <div className="flex flex-row justify-center items-center min-h-svh">
+      <div className="flex flex-col gap-6 items-center">
+        <h1 className="text-5xl font-bold">The Feasibility</h1>
+        <iframe
+          src="https://docs.google.com/presentation/d/e/2PACX-1vQAw5-8V2xrkrYlycTMhnIosr_IwMWLlGy_N9HzUQq_3p90dqG6qAomO0JRD8r0e7RcD8q1na9ufneH/embed?start=false&loop=false&delayms=3000"
+          frameBorder="0"
+          width="1430"
+          height="839"
+          allowFullScreen={true}
+          mozallowfullscreen="true"
+          webkitallowfullscreen="true"
+        ></iframe>
       </div>
     </div>
   );
