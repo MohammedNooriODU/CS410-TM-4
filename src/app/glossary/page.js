af611dc3a281a086e855051b594072eb20c20ec7
Qin Davis <qdavi007@odu.edu>
2024-12-09 22:38:27 -0500

    filled out glossary, fixed main page type "don" -> "don't"

--- src/app/glossary/page.js
+++ src/app/glossary/page.js
@@ -6,27 +6,27 @@ export default function Glossary() {
           <h1 className="text-5xl font-bold">Glossary</h1>
           <p className="pt-6 py-2 text-lg text-black">
             <span className="text-black text-xl font-bold">Next.js</span>:
-            DESCRIBE WHAT IT IS
+            A React framework that simplifies the process of building fast and scalable web apps.
           </p>
           <p className="py-2 text-lg text-black">
-            <span className="text-black text-xl font-bold">HTML</span>: DESCRIBE
-            WHAT IT IS
+            <span className="text-black text-xl font-bold">HTML</span>: 
+            A markup language used to create and structure content on web pages.
           </p>
           <p className="py-2 text-lg text-black">
-            <span className="text-black text-xl font-bold">CSS</span>: DESCRIBE
-            WHAT IT IS
+            <span className="text-black text-xl font-bold">CSS</span>: 
+            A stylesheet language use to control the layout, presentation, and visual styling of web pages. Works in conjunction with HTML to enhance appearance of web content.
           </p>
           <p className="py-2 text-lg text-black">
-            <span className="text-black text-xl font-bold">IDE</span>: DESCRIBE
-            WHAT IT IS
+            <span className="text-black text-xl font-bold">IDE</span>: 
+            A software application that provides developers a space with proper tools and features to write, debug, and manage code. The central hub for software development. 
           </p>
           <p className="py-2 text-lg text-black">
             <span className="text-black text-xl font-bold">MongoDB</span>:
-            DESCRIBE WHAT IT IS
+            A non-relation NoSql database. Data is stored in flexible document-oriented format versus relational databases' table format. Designed to handle large volumes of unscructured or semi-structured data.
           </p>
           <p className="py-2 text-lg text-black">
-            <span className="text-black text-xl font-bold">API</span>: DESCRIBE
-            WHAT IT IS
+            <span className="text-black text-xl font-bold">API</span>: 
+            Set of protocols, routines, tools that allow different software applications or systems to communicate with eachother. Defines methods and data formats that applications can use to request and exchange information. Essentially allows develops to interact with the functionality or data provided by another service/system.  
           </p>
         </div>
       </div>
